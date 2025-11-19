7ca7c1ea677717e5234b757b1476598a884b9c64
msmithfl <msmithfl@icloud.com>
2025-11-19 06:00:38 -0500

    setup mobile not supported page

--- frontend/Top4Battle/src/App.tsx
+++ frontend/Top4Battle/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import GameScreen from './components/GameScreen'
+import MobileNotSupported from './components/MobileNotSupported'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/game" element={<GameScreen />} />
+      <Route path="/mobile-not-supported" element={<MobileNotSupported />} />
     </Routes>
   )
 }
