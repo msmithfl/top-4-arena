2f04010f087eb987e21d323ebb5fc51456e9b966
jesmith0 <user@github.com>
2025-11-21 02:05:52 -0500

    add some opacity to the background when modals are open

--- frontend/Top4Battle/src/components/EndGameButton.tsx
+++ frontend/Top4Battle/src/components/EndGameButton.tsx
@@ -23,7 +23,7 @@ const EndGameButton: React.FC = () => {
 
       {isOpen && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50 p-4"
+          className="fixed inset-0 bg-black/90 flex items-center justify-center z-50 p-4"
           onClick={() => setIsOpen(false)}
         >
           <div
